6f000633bcbafe8731efbc18a82757d107a60c31
Tobias Høegh <tobias@tujo.no>
2026-01-12 22:25:56 +0100

    fix: handle TS/TSX directory checks without forcing `/index`

--- babel-plugin-fully-specified/__tests__/artifacts/module.tsx
+++ babel-plugin-fully-specified/__tests__/artifacts/module.tsx
@@ -1,11 +1,13 @@
 import '@babel-plugin-fully-specified/test-package'
 import '@babel-plugin-fully-specified/test-package/subdir'
 import './modules/tsx/foo'
+import './modules/tsx/hybrid'
 import './modules/tsx'
 import './modules/tsx/styles.min.css'
 import './modules/tsx/bar.ts'
 import('./modules/tsx/foo')
 
 export * from './modules/tsx/foo'
+export * from './modules/tsx/hybrid'
 export * as name from './modules/tsx'
 export { foo } from './modules/tsx/foo'
